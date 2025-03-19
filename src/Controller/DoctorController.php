eda49fb9be97a5f6ff1a03e2af5b53b6692faf80
Mohamed Alshahoud <ay1665ay@gmail.com>
2025-03-19 17:11:19 +0100

    Modification de l'affichage de la page du Médecin

--- src/Controller/DoctorController.php
+++ src/Controller/DoctorController.php
@@ -2,17 +2,23 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class DoctorController extends AbstractController
 {
-    #[Route('/doctor', name: 'app_doctor')]
-    public function index(): Response
+    #[Route('/doctor/{id}/appointments', name: 'doctor_appointments')]
+    public function consultation($id, EntityManagerInterface $em): Response
     {
-        return $this->render('doctor/index.html.twig', [
-            'controller_name' => 'DoctorController',
+        // Récupérer les rendez-vous pour le médecin en fonction de son ID
+
+        $doctorAppointments = $em->getRepository(Rendezvous::class)->findBy(['doctor' => $id]);
+
+        // Retourner les résultats dans une vue
+        return $this->render('doctor/appointments.html.twig', [
+            'appointments' => $doctorAppointments,
         ]);
     }
 }
