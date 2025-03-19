411a74d4f52edd871d44866b6a7d4b9aabfe5609
Mohamed Alshahoud <ay1665ay@gmail.com>
2025-03-19 16:17:45 +0100

    Ajout de sécurité et création de la page de réservation

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,13 +4,11 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
-class HomeController extends AbstractController
+final class HomeController extends AbstractController
 {
-    /**
-     * @Route("/", name="home")
-     */
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig');
